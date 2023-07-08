3f26d8a4e3a492e06d0895e9d7b41c0cb83db5e6
Kevin Gil <93kevingil@gmail.com>
2023-07-08 14:19:38 -0300

    comment out timer script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -4,39 +4,39 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-  public float timeValue = 90;
-  public bool timerOn = false;
+  // public float timeValue = 90;
+  // public bool timerOn = false;
 
-  public TextAlignment TimerTxt;
+  // public TextAlignment TimerTxt;
 
-  void Start()
-  {
-    timerOn = true;
-  }
+  // void Start()
+  // {
+  //   timerOn = true;
+  // }
 
-  void Update()
-  {
-    if (timerOn)
-    {
-      if (timeValue > 0)
-      {
-        timeValue -= Time.deltaTime;
-        updateTimer(timeValue);
-      }
-      else
-      {
-        Debug.Log("Time is UP!");
-        timeValue = 0;
-        timerOn = false;
-      }
-    }
-  }
+  // void Update()
+  // {
+  //   if (timerOn)
+  //   {
+  //     if (timeValue > 0)
+  //     {
+  //       timeValue -= Time.deltaTime;
+  //       updateTimer(timeValue);
+  //     }
+  //     else
+  //     {
+  //       Debug.Log("Time is UP!");
+  //       timeValue = 0;
+  //       timerOn = false;
+  //     }
+  //   }
+  // }
 
-  void updateTimer(float currentTime)
-  {
-    float minutes = Mathf.FloorToInt(currentTime / 60);
-    float seconds = Mathf.FloorToInt(currentTime % 60);
+  // void updateTimer(float currentTime)
+  // {
+  //   float minutes = Mathf.FloorToInt(currentTime / 60);
+  //   float seconds = Mathf.FloorToInt(currentTime % 60);
 
-    TimerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
-  }
+  //   TimerTxt.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+  // }
 }
