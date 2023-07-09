18265fea8de131373144018ca0e905a70a38558b
Samantha Gittemeier <sammig249@gmail.com>
2023-07-09 00:51:49 -0500

    Added boss sound effects

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -14,29 +14,43 @@ public class Boss : MonoBehaviour
     [SerializeField]
     private Animator _bossAnim;
 
+    [SerializeField]
+    private AudioSource _audioPlayer;
+
+    [SerializeField]
+    private AudioClip[] _audioClips;
+
     // Start is called before the first frame update
     void Start()
     {
         _bossHealth = 10;
         _bossHealthText = GameObject.Find("BossHealth").GetComponent<TMP_Text>();
         _bossAnim = gameObject.GetComponent<Animator>();
+        _audioPlayer = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         _bossHealthText.text = _bossHealth.ToString();
-        if (_bossHealth <= 0)
-        {
-            _bossAnim.SetBool("BossDead", true);
-            Destroy(this.gameObject, 2.5f);
-        }
     }
 
     public void TakeDamage(int _damageAmount)
     {
         _bossHealth -= _damageAmount;
-        StartCoroutine(ResetDamageAnimation());
+        if (_bossHealth <= 0)
+        {
+            _bossAnim.SetBool("BossDead", true);
+            _audioPlayer.clip = _audioClips[0];
+            _audioPlayer.Play();
+            Destroy(this.gameObject, 2.5f);
+        }
+        else if (_bossHealth > 0)
+        {
+            _audioPlayer.clip = _audioClips[1];
+            _audioPlayer.Play();
+            StartCoroutine(ResetDamageAnimation());
+        }
     }
 
     IEnumerator ResetDamageAnimation()
